272337c35ea0d6622fbe5b34905160170144ed7b
Runè van der Merwe <runevdm3@gmail.com>
2023-11-13 12:05:36 +0100

    project initialized with renv

--- Scripts/first_script.R
+++ Scripts/first_script.R
@@ -1,1 +1,10 @@
 # my first github script
+library(renv)
+## renv::init()  ## only once; create lock file in respo
+renv::restore() ## restore the versions of the packages used to develop this project
+
+x<- c(1,2,3,4,5)
+y<- c(1,4,3,6,9)
+data<-data.frame(x,y)
+data
+
