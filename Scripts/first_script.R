6332f9090cc1135cea8baf622386e90f60c71bd6
Runè van der Merwe <runevdm3@gmail.com>
2023-11-13 13:07:12 +0100

    Made a graph

--- Scripts/first_script.R
+++ Scripts/first_script.R
@@ -7,7 +7,7 @@ renv::restore() ## restore the versions of the packages used to develop this pro
 ##renv::snapshot() # only once to update libraries
 
 library(tidyverse)
-
+##install.packages("tidyverse",dependencies = T,repos="http://cran.us.r-project.org")
 x<- c(1,2,3,4,5)
 y<- c(1,4,3,6,9)
 data<-data.frame(x,y)
